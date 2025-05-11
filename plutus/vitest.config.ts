db14af35d8997eb78d79da901ba5dd8ddeb3824c
sergant-booblik <serj.solntsev@gmail.com>
2025-05-11 12:28:41 +0300

    Add test coverage

--- plutus/vitest.config.ts
+++ plutus/vitest.config.ts
@@ -9,6 +9,11 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'istanbul',
+        reporter: ['text', 'html'],
+        exclude: ['node_modules/', 'tests/', 'dist/'],
+      },
     },
   }),
 );
